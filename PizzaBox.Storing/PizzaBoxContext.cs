f4b24b161b27d435eb672a83d7d9005be4398b29
Glenn Edinburgh <gledinburgh@gmail.com>
2021-05-07 21:37:45 -0500

    switching branch

--- PizzaBox.Storing/PizzaBoxContext.cs
+++ PizzaBox.Storing/PizzaBoxContext.cs
@@ -7,15 +7,32 @@ namespace PizzaBox.Storing
   {
 
     public DbSet<Crust> Crusts { get; set; }
+    public DbSet<Crust> Toppings { get; set; }
+    public DbSet<Crust> Sizes { get; set; }
+    public DbSet<Crust> Pizzas { get; set; }
+    public DbSet<Crust> Stores { get; set; }
+    public DbSet<Crust> Customers { get; set; }
 
     public PizzaBoxContext(DbContextOptions options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+      builder.Entity<AStore>().HasKey(e => e.EntityId);
+      builder.Entity<ChicagoStore>().HasBaseType<AStore>();
+      builder.Entity<NewYorkStore>().HasBaseType<AStore>();
+
+      builder.Entity<APizza>().HasKey(e => e.EntityId);
+      builder.Entity<CustomPizza>().HasBaseType<APizza>();
+      builder.Entity<MeatPizza>().HasBaseType<APizza>();
+      builder.Entity<VeggiePizza>().HasBaseType<APizza>();
+
       builder.Entity<Crust>().HasKey(e => e.EntityId);
 
+      builder.Entity<Order>().HasKey(e => e.EntityId);
+      builder.Entity<Size>().HasKey(e => e.EntityId);
+      builder.Entity<Topping>().HasKey(e => e.EntityId);
+
+      builder.Entity<Customer>().HasKey(e => e.EntityId);
     }
-
   }
-
 }
